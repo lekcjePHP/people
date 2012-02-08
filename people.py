b89d77c1a805c17f07e0085c88b983f969a89807
Art.py <lekcjephp@gmail.com>
2012-02-08 17:58:55 +0000

    zakonczylismy projket sukcesem, makieta ludzi

--- people.py
+++ people.py
@@ -21,11 +21,14 @@ class Human(SuperHuman):
 		self.parents.append(Aparent2)
 		self.race = Arace
 		self.sex = Asex
-	def card(self):
+	def card(self, first_run = 0):
 		print("Jestem zwyklym czlowiekiem, mam na imie "+self.name)
 		print("Moi rodzice: ")
 		self.parents[0].card()
 		self.parents[1].card()
+		if first_run == 0:
+			for x in self.sibling:
+				x.card(first_run=1)
 		print("Moja rasa to: "+self.race)
 		print("Moja plec to: "+self.sex)
 		print("Moja profesja to: "+self.profession)
@@ -34,11 +37,16 @@ adam =  SuperHuman('adam', 'white', 'male')
 ewa =  SuperHuman('ewa', 'white', 'female')
 
 antek = Human('antek', adam, ewa, 'white', 'male')
+janek = Human('janek',adam, ewa, 'white', 'male')
+ola = Human('ola', adam, ewa, 'white', 'female')
+antek.sibling.append(janek)
+antek.sibling.append(ola)
+janek.sibling.append(antek)
+janek.sibling.append(ola)
+ola.sibling.append(janek)
+ola.sibling.append(antek)
 
-antek.card()
-print()
-adam.card()
-
+janek.card()
 
 
 antek.profession = 'stolarz'
